0842107c7d0253f953c862623689f9c4e4adde65
Sonya <124429607+sforsuper@users.noreply.github.com>
2023-03-12 15:20:24 +0300

    Add files via upload

---
+++ lab 4 task 3.cpp
@@ -0,0 +1,39 @@
+﻿// ConsoleApplication4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+	int matrix[101][101];
+	int n, m; 
+	std::cin >> n >> m;
+	int min = INT_MAX;
+	int index = -1;
+	for (int i = 0; i < n; i++) {
+		int result = 0;
+		for (int j = 0; j < m; j++) {
+			std::cin >> matrix[i][j]; 
+			result += matrix[i][j];
+		}
+		if (std::abs(result) < min) {
+			min = std::abs(result);
+			index = i;
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (index == i) {
+			for (int j = 0; j < m; j++) {
+				matrix[i][j] = 0;
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			std::cout << matrix[i][j] << " ";
+		}
+		std::cout << "\n";
+	}
+	
+}
