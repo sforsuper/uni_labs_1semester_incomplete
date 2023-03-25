244d2cf0a96e503cace5c73a61536e5cc8b3c96b
Sonya <124429607+sforsuper@users.noreply.github.com>
2023-03-25 14:43:39 +0200

    Add files via upload

---
+++ lab 6 main.cpp
@@ -0,0 +1,26 @@
+﻿#include "func.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace math;
+
+int main() {
+	MatrixSize size;
+	int**m = read_matrix(size);
+	unsigned left_col;
+	unsigned right_col;
+	print(m, size);
+
+	if (has_equal_columns(m, size, left_col, right_col)) {
+		std::cout << "has equal columns : " << left_col << " " << right_col << std::endl;
+		if (has_prime(m, size, left_col)) {
+			std::cout << "has primes" << std::endl;
+			sort_rows_by_abs_summ(m, size);
+		}
+	}
+
+	print(m, size);
+
+	free_memory(m, size);
+	return 0;
+}
